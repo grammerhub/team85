8cb0bc14beeddfa6b4759e683a0ff1e40192d80c
Jimmy <jrv1295@gmail.com>
2023-04-14 17:31:35 -0400

    Navbar and Chat Features

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Chat from "./pages/ChatPage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
+      
     <Router>
+      <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path='/tutor' element={<Chat />} />
